4e481579930e9725588d3de4a675a5687a4bef7c
pratikshatiwari <47111738+pratikshatiwari@users.noreply.github.com>
2024-12-24 22:55:34 +0530

    Create TestUnusedVariables.cs

---
+++ src/Web/TestUnusedVariables.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace WebProject
+{
+    public class TestUnusedVariables
+    {
+        public void TestMethod()
+        {
+            // Used variables
+            int usedVariable = 5;
+            Console.WriteLine($"Used variable: {usedVariable}");
+
+            // Unused variables
+            int unusedVariable = 10; // Should be flagged
+            string unusedString = "Hello, CodeQL!"; // Should be flagged
+
+            // A variable declared but used in a conditional block
+            int conditionallyUsedVariable = 20;
+            if (conditionallyUsedVariable > 0) // This usage will prevent it from being flagged
+            {
+                Console.WriteLine("Conditionally used variable.");
+            }
+        }
+    }
+}
